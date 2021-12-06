de6509a7868cabead15d6568f3cb5cd3c8447042
juan9507 <juandavidnaranjo75@gmail.com>
2021-12-06 14:24:19 -0500

    Actualizacion mutacion proyeto add lideres,estudiantes,avances

--- backend-proyecto/lib/mutations.js
+++ backend-proyecto/lib/mutations.js
@@ -5,6 +5,7 @@ const { ObjectId } = require("mongodb");
 const errorHandler = require("./errorHandler");
 
 module.exports = {
+  // Crea un proyecto
   createProyecto: async (root, { input }) => {
     const defaults = {
       lideres: [],
@@ -23,6 +24,7 @@ module.exports = {
     }
     return newProyecto;
   },
+  // Edita un proyecto
   editProyecto: async (root, { _id, input }) => {
     let db;
     let proyecto;
@@ -39,6 +41,7 @@ module.exports = {
     }
     return proyecto;
   },
+  // Elimina un proyecto
   removeProyecto: async (root, { _id }) => {
     let db;
     let proyecto = [];
@@ -50,5 +53,111 @@ module.exports = {
       errorHandler(error);
     }
     return proyecto;
+  },
+  // Agrega lider a un proyecto
+  addLider: async (root, { proyectoId, input }) => {
+    let db;
+    let proyecto;
+    let newProyecto;
+    try {
+      db = await connectDb();
+      proyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(proyectoId) });
+      if (!proyecto) throw new Error("El proyecto no existe");
+      await db
+        .collection("proyectos")
+        .updateOne(
+          { _id: ObjectId(proyectoId) },
+          { $addToSet: { lideres: input } },
+          { new: true }
+        );
+      newProyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(proyectoId) });
+    } catch (error) {
+      errorHandler(error);
+    }
+    return newProyecto;
+  },
+  // Agrega estudiantes a un proyecto
+  addEstuadiante: async (root, { proyectoId, usuarioId, input }) => {
+    let db;
+    let proyecto;
+    let usuario;
+    let rol;
+    let newProyecto;
+    const defaults = {
+      fecha_egreso: "",
+    };
+    const newEstudiante = Object.assign(defaults, input);
+    try {
+      db = await connectDb();
+      proyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(proyectoId) });
+      usuario = await db
+        .collection("usuarios")
+        .findOne({ _id: ObjectId(usuarioId) });
+      if (!proyecto || !usuario.rol === "estudiante")
+        throw new Error("El proyecto o estudiante no existe");
+      await db
+        .collection("proyectos")
+        .updateOne(
+          { _id: ObjectId(proyectoId) },
+          { $addToSet: { inscripciones: newEstudiante } },
+          { new: true }
+        );
+      newProyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(proyectoId) });
+    } catch (error) {
+      errorHandler(error);
+    }
+    return newProyecto;
+  },
+  // Agrega avances a un proyecto
+  addAvance: async (root, { proyectoId, documento, input }) => {
+    let db;
+    let proyecto;
+    let newProyecto;
+    let estudianteData;
+    let validar = false;
+    const defaults = {
+      observaciones: "",
+    };
+    const newAvance = Object.assign(defaults, input);
+    try {
+      db = await connectDb();
+      proyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(proyectoId) });
+      estudianteData = proyecto.inscripciones.map((inscripciones) => {
+        if (
+          inscripciones.documento == `${documento}` &&
+          inscripciones.estado_inscripcion == "aceptado"
+        ) {
+          validar = true;
+        }
+        // return false;
+      });
+      if (!proyecto || validar == false)
+        throw new Error(
+          "El proyecto o estudiante no existen, o el estudiante no a sido aceptado"
+        );
+      await db
+        .collection("proyectos")
+        .updateOne(
+          { _id: ObjectId(proyectoId) },
+          { $addToSet: { avances: newAvance } },
+          { new: true }
+        );
+      newProyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(proyectoId) });
+    } catch (error) {
+      errorHandler(error);
+    }
+    return newProyecto;
   },
 };
