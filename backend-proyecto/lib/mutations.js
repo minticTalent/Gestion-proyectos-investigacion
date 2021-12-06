bd4c86f5d9ae5337a0d934481c5af47512451edc
juan9507 <juandavidnaranjo75@gmail.com>
2021-12-05 22:36:25 -0500

    Mutaciones colection proyecto 5/12/2021

--- backend-proyecto/lib/mutations.js
+++ backend-proyecto/lib/mutations.js
@@ -1,2 +1,54 @@
-'use strict' 
+"use strict";
 // Implementar las mutaciones
+const connectDb = require("./db");
+const { ObjectId } = require("mongodb");
+const errorHandler = require("./errorHandler");
+
+module.exports = {
+  createProyecto: async (root, { input }) => {
+    const defaults = {
+      lideres: [],
+      inscripciones: [],
+      avances: [],
+    };
+    const newProyecto = Object.assign(defaults, input);
+    let db;
+    let proyecto;
+    try {
+      db = await connectDb();
+      proyecto = await db.collection("proyectos").insertOne(newProyecto);
+      newProyecto._id = proyecto.insertedId;
+    } catch (error) {
+      errorHandler(error);
+    }
+    return newProyecto;
+  },
+  editProyecto: async (root, { _id, input }) => {
+    let db;
+    let proyecto;
+    try {
+      db = await connectDb();
+      await db
+        .collection("proyectos")
+        .updateOne({ _id: ObjectId(_id) }, { $set: input });
+      proyecto = await db
+        .collection("proyectos")
+        .findOne({ _id: ObjectId(_id) });
+    } catch (error) {
+      errorHandler(error);
+    }
+    return proyecto;
+  },
+  removeProyecto: async (root, { _id }) => {
+    let db;
+    let proyecto = [];
+    try {
+      db = await connectDb();
+      await db.collection("proyectos").deleteOne({ _id: ObjectId(_id) });
+      proyecto = await db.collection("proyectos").find().toArray();
+    } catch (error) {
+      errorHandler(error);
+    }
+    return proyecto;
+  },
+};
--- backend-proyecto/lib/resolvers.js
+++ backend-proyecto/lib/resolvers.js
@@ -1,11 +1,11 @@
-'use strict' 
+"use strict";
 // Resolvers ya queda funcionando para su uso
-const mutations = require('./mutations')
-const queries = require('./queries')
-const types = require('./types')
+const mutations = require("./mutations");
+const queries = require("./queries");
+const types = require("./types");
 module.exports = {
-    Query: queries,
-    // Mutation: mutations,
-    // Por si se utiliza los type descomentar
-     ...types
-}
+  Query: queries,
+  Mutation: mutations,
+  // Por si se utiliza los type descomentar
+  ...types,
+};
